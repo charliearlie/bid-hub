ff6c85e5798c157fc31a3775e483ea7f7d7bbfeb
charlie <cw5790@gmail.com>
2022-12-12 23:44:45 +0000

    Update reference to sting to kebab case file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,2 @@
-export { default as toKebabCase } from './stringToKebabCase';
+export { default as toKebabCase } from './string-to-kebab-case';
 export { default as ResolverError } from './types/ResolverError';
