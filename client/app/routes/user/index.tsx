e5184bf6c2e324c3bcf01b77d591af7b55a06883
charlie <cw5790@gmail.com>
2023-01-24 23:26:38 +0000

    Add requestWithCredentials function to gql-request

--- client/app/routes/user/index.tsx
+++ client/app/routes/user/index.tsx
@@ -1,21 +1,6 @@
-import { json } from "@remix-run/node";
-import { Outlet, useLoaderData } from "@remix-run/react";
-import { ME_QUERY } from "~/gql/queries/me";
-import { requestClient } from "~/util/gql-request";
+import { Outlet } from "@remix-run/react";
 
-export async function loader({ request }: any) {
-  // Surely must be a way to do this without using this same code in every loader?
-  const cookieHeader = request.headers.get("Cookie");
-  const response = await requestClient.request(ME_QUERY, undefined, {
-    Cookie: cookieHeader,
-  });
-  return json({ data: response });
-}
-
-export default function UserRoute(req: Request) {
-  const {
-    data: { me },
-  } = useLoaderData();
+export default function UserRoute() {
   return (
     <div>
       <main>
--- client/app/util/gql-request.ts
+++ client/app/util/gql-request.ts
@@ -1,5 +1,25 @@
 import { GraphQLClient } from "graphql-request";
+import { getUserIdFromSession } from "~/session.server";
+export * from "graphql-request";
+export const requestClient = new GraphQLClient(
+  "http://localhost:4000/graphql",
+  {
+    credentials: "include",
+  }
+);
 
-export const requestClient = new GraphQLClient(process.env.BACKEND_ENDPOINT, {
-  credentials: "include",
-});
+export const requestWithCredentials = async (
+  queryOrMutation: string,
+  request: Request
+) => {
+  const cookieHeader = request.headers.get("Cookie");
+  const userId = await getUserIdFromSession(request);
+
+  if (!userId) {
+    return "/login";
+  }
+
+  return await requestClient.request(queryOrMutation, undefined, {
+    ...(cookieHeader && { Cookie: cookieHeader }),
+  });
+};
