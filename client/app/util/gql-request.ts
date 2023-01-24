70c19479ac60509088d03f6a9514cd3a2cddf888
charlie <cw5790@gmail.com>
2023-01-24 23:35:57 +0000

    Use env variables

--- client/app/util/gql-request.ts
+++ client/app/util/gql-request.ts
@@ -1,12 +1,9 @@
 import { GraphQLClient } from "graphql-request";
 import { getUserIdFromSession } from "~/session.server";
 export * from "graphql-request";
-export const requestClient = new GraphQLClient(
-  "http://localhost:4000/graphql",
-  {
-    credentials: "include",
-  }
-);
+export const requestClient = new GraphQLClient(process.env.BACKEND_ENDPOINT, {
+  credentials: "include",
+});
 
 export const requestWithCredentials = async (
   queryOrMutation: string,
