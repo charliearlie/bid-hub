97402651de753942089b57ebf730bfa95db85182
charlie <cw5790@gmail.com>
2023-01-28 23:12:49 +0000

    Add extra types to env interface

--- client/app/env.d.ts
+++ client/app/env.d.ts
@@ -1,6 +1,9 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     BACKEND_ENDPOINT: string;
+    CLOUDINARY_KEY: string;
+    CLOUDINARY_SECRET: string;
+    CLOUDINARY_URL: string;
     SESSION_SECRET: string;
   }
 }
