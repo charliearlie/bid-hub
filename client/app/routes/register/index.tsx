560d1fc351f080b20c4eae490917d2c39d5bf376
charlie <cw5790@gmail.com>
2023-02-26 01:56:28 +0000

    Add button component to register page

--- client/app/routes/register/index.tsx
+++ client/app/routes/register/index.tsx
@@ -6,6 +6,7 @@ import FormField from "~/components/form/form-field";
 import { createUserSession } from "~/session.server";
 import { gql, requestClient } from "~/gql/util/gql-request";
 import Spinner from "~/components/spinner";
+import Button from "~/components/button";
 
 const REGISTER_USER = gql`
   mutation Register($email: String!, $password: String!, $username: String!) {
@@ -91,9 +92,9 @@ export default function RegisterRoute() {
           <FormField label="Email" name="email" type="text" />
           <FormField label="Password" name="password" type="password" />
           <div className="flex justify-between">
-            <button className="w-25 rounded bg-violet-700 px-3 py-2 text-lg font-semibold text-white hover:bg-violet-900">
+            <Button className="w-25" variant="primary">
               {transition.state !== "idle" ? <Spinner /> : "Sign up"}
-            </button>
+            </Button>
             <Link
               className="px-0 py-2 font-semibold text-blue-700 hover:text-slate-500"
               to="/login"
