c9f239fd06162dbfc18c2c3cf311c8408249ad81
charlie <cw5790@gmail.com>
2023-03-01 22:49:14 +0000

    Add new field to resolver error

--- server/src/utils/types/ResolverError.ts
+++ server/src/utils/types/ResolverError.ts
@@ -3,7 +3,10 @@ import { Field, ObjectType } from 'type-graphql';
 @ObjectType()
 export default class ResolverError {
   @Field(() => String)
-  field: string;
+  resolver?: string;
+
+  @Field(() => String)
+  field?: string;
 
   @Field(() => String)
   message: string;
