af1505fad30c0128cfe646e050c6daf692427a91
charlie <cw5790@gmail.com>
2023-03-01 22:48:18 +0000

    Make skeleton item page where bids can be placed

--- client/app/gql/queries/item-by-id.query.tsx
+++ client/app/gql/queries/item-by-id.query.tsx
@@ -3,7 +3,28 @@ export const ITEM_BY_ID_QUERY = gql`
   query GetItemById($itemId: Float!) {
     getItemById(id: $itemId) {
       item {
+        id
         name
+        hasBiddingEnabled
+        startingPrice
+        winningBid
+        formattedWinningBid
+        bids {
+          amount
+        }
+        condition
+        categories {
+          title
+          id
+        }
+        formattedBuyItNowPrice
+        formattedStartingPrice
+        description
+        bidCount
+        imageUrl
+        seller {
+          username
+        }
       }
       success
       errors {
