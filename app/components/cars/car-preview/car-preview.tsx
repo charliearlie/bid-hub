24b220a45aaf275419919fc08727a4d80f05c4df
charlie <cw5790@gmail.com>
2023-03-13 23:21:24 +0000

    Update car preview component

--- app/components/cars/car-preview/car-preview.tsx
+++ app/components/cars/car-preview/car-preview.tsx
@@ -6,18 +6,26 @@ type CarPreviewProps = {
 };
 
 export default function CarPreview({ car }: CarPreviewProps) {
-  const { manufacturerName, model, slug } = car;
+  const { manufacturerName, model, previewImage, slug, variation, year } = car;
 
   return (
-    <Link to={`/car/${slug}`} className="flex gap-4 py-4 px-4">
-      <div className="h-24 w-24 flex-none">
-        <img src="https://ih1.redbubble.net/image.1003426384.0291/st,small,507x507-pad,600x600,f8f8f8.jpg" />
-      </div>
-      <div className="flex-auto">
-        <h3 className="h-9 overflow-hidden font-black leading-none">
-          {manufacturerName}
-        </h3>
-        <p className="text-xs font-semibold">{model}</p>
+    <Link
+      to={`/car/${slug}`}
+      aria-label={`View ${manufacturerName} ${model}`}
+      className="flex w-full cursor-pointer flex-col items-center gap-2 rounded outline outline-2 outline-gray-800 hover:opacity-70 focus:outline-4 focus:outline-blue-400"
+    >
+      <img
+        className="h-60 w-full object-cover"
+        loading="lazy"
+        src={previewImage || "defaultImage"}
+        alt={`${manufacturerName} ${model}`}
+      />
+      <div className="py-2 text-center">
+        <h3 className="text-3xl font-black leading-none">{manufacturerName}</h3>
+        <p className="text-2xl font-semibold">
+          {model} {variation}
+        </p>
+        <p className="font-semi-bold text-xl">{year}</p>
       </div>
     </Link>
   );
--- app/components/cars/car-preview/previews.tsx
+++ app/components/cars/car-preview/previews.tsx
@@ -4,9 +4,9 @@ import CarPreview from "../car-preview/car-preview";
 
 export default function Previews({ cars }: { cars: Car[] }) {
   return (
-    <ul className="grid grid-cols-1 lg:grid-cols-3">
-      {cars.map((car) => (
-        <li key={car.slug}>
+    <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {cars.map((car, idx) => (
+        <li className="rounded" key={car.slug}>
           <CarPreview car={car} />
           <Divider />
         </li>
