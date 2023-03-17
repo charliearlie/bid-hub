ed2678d8a7b274a3e24851b4777f08b2ea7e73de
charlie <cw5790@gmail.com>
2023-03-17 00:37:17 +0000

    Add info rows to display car data

---
+++ app/components/cars/info/body-and-chassis-info-row.tsx
@@ -0,0 +1,32 @@
+import { BodyAndChassis, Engine, Powertrain } from "@prisma/client";
+import CarInfoRow from "./car-info-row";
+
+type Props = {
+  bodyAndChassis: BodyAndChassis;
+};
+// todo: We could maybe map the key to a value to make this usable across all info rows
+export default function BodyAndChassisInfoRows({ bodyAndChassis }: Props) {
+  const {
+    bodyMaterial,
+    brakes,
+    doorType,
+    layout,
+    seats,
+    steering,
+    suspension,
+    tyres,
+  } = bodyAndChassis;
+  return (
+    <ul>
+      {layout && <CarInfoRow label="Layout" value={layout} />}
+      {bodyMaterial && (
+        <CarInfoRow label="Body material" value={bodyMaterial} />
+      )}
+      {doorType && <CarInfoRow label="Doors" value={doorType} />}
+      {seats && <CarInfoRow label="Seats" value={seats} />}
+      {brakes && <CarInfoRow label="Brakes" value={brakes} />}
+      {steering && <CarInfoRow label="Steering" value={steering} />}
+      {tyres && <CarInfoRow label="Tyres" value={tyres} />}
+    </ul>
+  );
+}
--- app/routes/car/$carSlug.tsx
+++ app/routes/car/$carSlug.tsx
@@ -1,13 +1,21 @@
 import { useLoaderData } from "@remix-run/react";
-import type { DataFunctionArgs, LoaderFunction} from "@remix-run/node";
-import { redirect } from "@remix-run/node";
+import type { DataFunctionArgs, LoaderFunction } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import Button from "~/components/common/button";
 import { getCarBySlug } from "~/services/cars.server";
 import type { Car } from "@prisma/client";
+import Card from "~/components/common/card/card";
+import CardContent from "~/components/common/card/card-content";
+import { redirect, useTypedLoaderData } from "remix-typedjson";
+import CardHeader from "~/components/common/card/card-header";
+import CardSubHeader from "~/components/common/card/card-subheader";
+import PowerTrainInfoRows from "~/components/cars/info/powertrain-info-rows";
+import EngineInfoRows from "~/components/cars/info/engine-info-row";
+import BodyAndChassisInfoRows from "~/components/cars/info/body-and-chassis-info-row";
 
 export const loader: LoaderFunction = async ({ params }: DataFunctionArgs) => {
   invariant(params.carSlug, "Expected params.carSlug");
+
   try {
     return await getCarBySlug(params.carSlug);
   } catch (error) {
@@ -16,25 +24,28 @@ export const loader: LoaderFunction = async ({ params }: DataFunctionArgs) => {
 };
 
 export default function CarSlugRoute() {
-  const car = useLoaderData<Car>();
+  const car = useTypedLoaderData<Car>();
   if (car) {
     const {
+      bodyAndChassis,
+      dimensions,
       model,
       variation,
       images,
       powertrain,
-      previewImage,
       topSpeed,
       zeroTo60,
+      zeroTo100,
       manufacturerName,
     } = car;
 
-    const image = images[0];
+    console.log("car", car);
+    const image = images?.[0];
     return (
       <main className="">
         <img
-          className="h-[32rem] w-full object-cover"
-          src={image.imageUrl || ""}
+          className=" h-72w-full object-cover lg:h-[32rem]"
+          src={image?.imageUrl || ""}
           alt={`${manufacturerName} ${model}`}
         />
         <div className="flex flex-col gap-8 p-4 md:flex-row md:p-12">
@@ -42,8 +53,41 @@ export default function CarSlugRoute() {
             <h1 className="text-2xl font-black md:text-4xl">
               {manufacturerName} {model}
             </h1>
-            <h2 className="text-2xl font-black md:text-4xl">{variation}</h2>
-            <p>0 - 60 in {zeroTo60} seconds</p>
+            <h2 className="text-2xl font-black md:text-4xl">{variation}</h2>I
+            could easily make the cards below tabs but they may vary in height
+            which will cause a disgusting content shift
+            <Card>
+              <CardHeader>Key figures</CardHeader>
+              <CardContent>
+                <div className="px-2">
+                  {zeroTo60 && <p>0 - 60 in {zeroTo60} seconds</p>}
+                  {zeroTo100 && <p>0 - 60 in {zeroTo100} seconds</p>}
+                  {topSpeed && <p>Top speed: {topSpeed}Mph</p>}
+                </div>
+              </CardContent>
+            </Card>
+            {powertrain && (
+              <Card>
+                <CardHeader>Powertrain</CardHeader>
+                <CardContent>
+                  <PowerTrainInfoRows powertrain={powertrain} />
+                  {powertrain.engine && (
+                    <>
+                      <CardSubHeader>Engine</CardSubHeader>
+                      <EngineInfoRows engine={powertrain.engine} />
+                    </>
+                  )}
+                </CardContent>
+              </Card>
+            )}
+            {bodyAndChassis && (
+              <Card>
+                <CardHeader>Body & chassis</CardHeader>
+                <CardContent>
+                  <BodyAndChassisInfoRows bodyAndChassis={bodyAndChassis} />
+                </CardContent>
+              </Card>
+            )}
             <Button variant="primary">Like</Button>
           </div>
         </div>
