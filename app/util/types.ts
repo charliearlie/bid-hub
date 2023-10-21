068e0bed31f6689fd32ec5baf17619eaeef28e9d
charlie <cw5790@gmail.com>
2023-10-21 23:37:40 +0100

    Create type which alters date types

--- app/util/types.ts
+++ app/util/types.ts
@@ -1,9 +1,49 @@
 import { Car, User } from "@prisma/client";
 
+export type RegisterForm = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export type LoginForm = {
+  email: string;
+  password: string;
+};
+
+export type EditUserForm = {
+  firstName?: string;
+  lastName?: string;
+  avatarUrl?: string;
+};
+
+export type ManufacturerForm = {
+  name: string;
+  country: string;
+  discipline?: string;
+};
+
+type RegisterErrors = {
+  email?: string;
+  password?: string;
+  username?: string;
+  server?: string;
+};
+
+export type RegisterResponse = {
+  errors: RegisterErrors;
+  success: boolean;
+};
+
 export type ReplaceDateProperties<T> = Omit<T, "createdAt" | "updatedAt"> & {
   createdAt: string;
   updatedAt: string;
 };
+
+export type CarPreviewType = Pick<
+  Car,
+  "manufacturerName" | "model" | "previewImage" | "slug" | "variation" | "year"
+>;
 
 export type CarType = ReplaceDateProperties<Car>;
 export type UserType = ReplaceDateProperties<User>;
