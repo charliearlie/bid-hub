f9c90a49d36523e6bfeac4b346b718f253c94dca
charlie <cw5790@gmail.com>
2023-10-22 19:12:16 +0100

    Add Shadcn button

--- app/components/common/ui/button.tsx
+++ app/components/common/ui/button.tsx
@@ -1,32 +1,56 @@
-import type { HTMLProps, ReactNode } from "react";
-import classNames from "classnames";
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
 
-type ButtonProps = {
-  children: ReactNode;
-  type?: "button" | "submit" | "reset" | undefined; // No idea why HTMLProps<HTMLButtonElement> doesn't cover this correctly
-  variant?: "primary" | "secondary" | "danger";
-};
-type Props = ButtonProps & HTMLProps<HTMLButtonElement>;
+import { cn } from "app/util/utils"
 
-export default function Button({
-  children,
-  className,
-  type,
-  variant = "primary",
-  ...props
-}: Props) {
-  const variantClass = classNames(
-    "button",
-    {
-      "button-primary": variant === "primary",
-      "button-secondary": variant === "secondary",
-      "button-danger": variant === "danger",
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline:
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-10 px-4 py-2",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
+      },
     },
-    className
-  );
-  return (
-    <button className={variantClass} type={type} {...props}>
-      {children}
-    </button>
-  );
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
 }
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = "Button"
+
+export { Button, buttonVariants }
