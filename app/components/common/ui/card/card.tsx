2e63d38c905683854d27b4c19f63f797db5d9b9d
charlie <cw5790@gmail.com>
2023-10-26 22:13:43 +0100

    Properly implement background colours on cards

--- app/components/common/ui/card/card.tsx
+++ app/components/common/ui/card/card.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 export default function Card({ children }: PropsWithChildren) {
   return (
-    <div className="my-2 flex flex-col rounded-lg bg-gray-800 shadow shadow-gray-900 focus:outline-4 focus:outline-blue-400">
+    <div className="rounded-lg border bg-card text-card-foreground shadow-sm dark:bg-accent">
       {children}
     </div>
   );
