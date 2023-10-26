f364525db1f8348c7be49ea8f14dd82a5938c0ba
charlie <cw5790@gmail.com>
2023-10-26 22:14:05 +0100

    Now use Login as a parent route

--- app/routes/_auth+/login.tsx
+++ app/routes/_auth+/login.tsx
@@ -1,27 +1,4 @@
-import React, { useRef } from "react";
-import {
-  Link,
-  useActionData,
-  useFormAction,
-  useNavigation,
-  useSearchParams,
-  useSubmit,
-} from "@remix-run/react";
-import {
-  redirect,
-  type LoaderFunction,
-  json,
-  DataFunctionArgs,
-} from "@remix-run/node";
-
-import Alert, { AlertType } from "~/components/common/ui/alert";
-import Form from "~/components/form/form";
-import FormField from "~/components/form/form-field";
-import Spinner from "~/components/spinner";
-import { Button } from "~/components/common/ui/button";
-import { generateMagicLink, login } from "~/services/user.server";
-import { getUser } from "~/services/session.server";
-import { SubmitButton } from "~/components/form/submit-button";
+import { Outlet } from "@remix-run/react";
 
 export const meta = () => {
   return [
@@ -30,139 +7,13 @@ export const meta = () => {
   ];
 };
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const user = await getUser(request);
-  // If there's already a user in the session, redirect to the home page
-  return user ? redirect("/") : null;
-};
-
-export async function action({ request }: DataFunctionArgs) {
-  const formData = await request.formData();
-
-  const intent = formData.get("intent");
-
-  const password = formData.get("password");
-  const emailOrUsername = formData.get("emailOrUsername");
-  const redirectTo = formData.get("redirectTo") as string; // We control it so always a string
-
-  if (typeof emailOrUsername !== "string") {
-    return json({
-      success: false,
-      error: `Invalid Form Data`,
-    });
-  }
-
-  if (intent === "magic") {
-    return await generateMagicLink(emailOrUsername as string);
-  }
-
-  // todo: not happy repeating this for both password and email. Sort it
-  if (typeof password !== "string" || typeof emailOrUsername !== "string") {
-    return json({
-      success: false,
-      error: `Invalid Form Data`,
-    });
-  }
-  return await login(
-    {
-      email: emailOrUsername,
-      password,
-    },
-    redirectTo
-  );
-}
-
 export default function LoginRoute() {
-  const emailInputRef = useRef<HTMLInputElement>(null);
-  const [searchParams] = useSearchParams();
-
-  const actionData = useActionData<typeof action>();
-  const navigation = useNavigation();
-  const formAction = useFormAction();
-
-  const isPending =
-    navigation.state !== "idle" &&
-    navigation.formAction === formAction &&
-    navigation.formMethod === "POST";
-
-  const submit = useSubmit();
-
-  const handleMagicLinkClick = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    e.stopPropagation();
-    e.preventDefault();
-
-    const formData = new FormData();
-    formData.append("intent", "magic");
-    formData.append("emailOrUsername", emailInputRef.current?.value as string);
-    submit(formData, { method: "post", replace: true });
-  };
-
   return (
     <div>
-      <h2 className="pt-4 pb-8 text-center text-3xl font-bold">
+      <h2 className="pt-4 pb-4 text-center text-3xl font-bold">
         Log in to Brake Neck
       </h2>
-      <Form
-        className=""
-        initialFormValues={{
-          emailOrUsername: "",
-          password: "",
-        }}
-        method="post"
-      >
-        {actionData?.error && (
-          <Alert
-            message="Invalid email, username or password"
-            type={AlertType.ERROR}
-          />
-        )}
-        {actionData?.success && (
-          <Alert
-            message={`A link has been sent to ${emailInputRef.current?.value}`}
-            type={AlertType.INFO}
-          />
-        )}
-        <FormField
-          label="Email or username" // Could default label to input name with a capital letter?
-          name="emailOrUsername"
-          type="text"
-          ref={emailInputRef}
-          required
-        />
-        <FormField label="Password" name="password" type="password" required />
-        <input
-          name="redirectTo"
-          type="text"
-          value={searchParams.get("redirectTo") || ""}
-          hidden
-        />
-        <div className="flex flex-col">
-          <SubmitButton
-            name="login"
-            variant="default"
-            status={isPending ? "pending" : "idle"}
-          >
-            Log in
-          </SubmitButton>
-          <Link
-            className="px-0 pb-2 font-semibold text-blue-500 hover:text-slate-500"
-            to="/forgot-password"
-          >
-            Forgot your password?
-          </Link>
-        </div>
-      </Form>
-      <Button
-        className="my-1 w-full"
-        name="magic"
-        onClick={handleMagicLinkClick}
-        type="button"
-        variant="secondary"
-      >
-        {isPending ? <Spinner /> : "Send Magic Link"}
-      </Button>
+      <Outlet />
     </div>
   );
 }
