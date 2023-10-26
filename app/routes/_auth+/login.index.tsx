d6952531552166799d450e49ac3b7d7d57441d34
charlie <cw5790@gmail.com>
2023-10-26 22:14:34 +0100

    Redirect to login/identifier when going to /login

---
+++ app/routes/_auth+/login.index.tsx
@@ -0,0 +1,7 @@
+// dashboard/index.tsx
+import type { LoaderFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+
+export const loader: LoaderFunction = async () => {
+  return redirect(`/login/identifier`);
+};
