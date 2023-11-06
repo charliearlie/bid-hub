f08e221175f84cbb960f86d00fe04e7c8beab2dc
charlie <cw5790@gmail.com>
2023-11-06 21:26:11 +0000

    Add shadcn date picker

---
+++ app/components/common/date-picker.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { cn } from "~/util/utils";
+import { Button } from "~/components/common/ui/button";
+import { Calendar } from "~/components/common/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/common/ui/popover";
+import { Input } from "./ui/input";
+
+type Props = {
+  name: string;
+  disabled?: {
+    before: Date;
+  };
+};
+
+export function DatePicker({ name }: { name: string }) {
+  const [date, setDate] = React.useState<Date>();
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "h-10 w-full justify-start bg-input text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+          disabled={{ before: new Date() }}
+        />
+      </PopoverContent>
+      <Input name={name} type="hidden" value={date?.toISOString()} />
+    </Popover>
+  );
+}
