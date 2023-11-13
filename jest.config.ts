ff59c36d358af8b9128cfe49a7a175d0b2728a1c
charlie <cw5790@gmail.com>
2023-11-13 23:09:46 +0000

    jest config now ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,11 @@
-import { pathsToModuleNameMapper } from "ts-jest";
 import type { JestConfigWithTsJest } from "ts-jest";
 
-// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
-// which contains the path mapping (ie the `compilerOptions.paths` option):
 import { compilerOptions } from "./tsconfig.json";
 
 const jestConfig: JestConfigWithTsJest = {
   testEnvironment: "jsdom",
   roots: ["<rootDir>"],
-  modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
+  modulePaths: [compilerOptions.baseUrl],
   moduleNameMapper: {
     "^~/(.*)$": "<rootDir>/app/$1",
   },
