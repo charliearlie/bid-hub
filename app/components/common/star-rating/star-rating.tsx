faff2ca611bbb3fc9bdd98a145824f97ce97210a
Charlie Waite <cw5790@gmail.com>
2023-11-23 18:54:35 +0000

    Add tabs and more information to user page (#84)
    * Add tabs and more information to user page
    
    * Remove console.log
    
    * Husky pre-commit hook should now regen routes

---
+++ app/components/common/star-rating/star-rating.tsx
@@ -0,0 +1,33 @@
+import { Star } from "lucide-react";
+
+import { cn } from "~/util/utils";
+
+export const RatingStars = ({ rating }: { rating: number | null }) => {
+  if (!rating) {
+    return (
+      <div className="flex items-center">
+        <p className="text-sm font-medium text-foreground opacity-60">
+          No ratings yet
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex items-center">
+      {[0, 1, 2, 3, 4].map((idx) => (
+        <Star
+          key={idx}
+          className={cn(
+            rating > idx
+              ? "fill-primary text-primary"
+              : "fill-text-foreground text-foreground",
+            "h-5 w-5 flex-shrink-0 text-sm md:text-base"
+          )}
+          strokeWidth={2}
+          aria-hidden="true"
+        />
+      ))}
+    </div>
+  );
+};
---
+++ app/components/reviews/review-list.tsx
@@ -0,0 +1,53 @@
+import { ReviewType } from "~/types";
+
+import { RatingStars } from "../common/star-rating/star-rating";
+import { Separator } from "../common/ui/separator";
+
+type Props = {
+  reviews: Array<ReviewType>;
+};
+
+export const ReviewList = ({ reviews }: Props) => {
+  return (
+    <>
+      <h3 className="sr-only">Customer Reviews</h3>
+
+      {reviews.map((review, reviewIdx) => (
+        <div
+          key={review.buyer.username}
+          className="flex space-x-4 text-sm text-foreground"
+        >
+          <div className="flex-none py-8">
+            <img
+              src={review.buyer.avatarUrl || ""}
+              alt=""
+              className="h-10 w-10 rounded-full bg-gray-100"
+            />
+          </div>
+          <div className="w-full">
+            {reviewIdx !== 0 ? <Separator /> : null}
+            <div className="py-8">
+              <h3 className="font-medium text-foreground">
+                {review.buyer.username}
+              </h3>
+              <p>
+                <time className="opacity-60" dateTime={review.createdAt}>
+                  {new Date(review.createdAt).toLocaleDateString()}
+                </time>
+              </p>
+
+              <div className="mt-2 flex items-center">
+                <RatingStars rating={review.rating} />
+              </div>
+              <p className="sr-only">{review.rating} out of 5 stars</p>
+
+              <div className="prose prose-sm mt-4 max-w-none text-foreground">
+                <p>{review.review}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
