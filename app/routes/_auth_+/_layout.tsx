08b04b9dddc344a241b7aa0dae3e4e6c90a6ca4d
charlie <cw5790@gmail.com>
2024-02-19 13:21:30 +0000

    Prevent authenticated users from visiting auth pages

--- app/routes/_auth_+/_layout.tsx
+++ app/routes/_auth_+/_layout.tsx
@@ -1,4 +1,4 @@
-import { json } from "@remix-run/node";
+import { DataFunctionArgs, json, redirect } from "@remix-run/node";
 import { Outlet, useLoaderData } from "@remix-run/react";
 import { HoneypotProvider } from "remix-utils/honeypot/react";
 
@@ -7,16 +7,25 @@ import CardContent from "~/components/common/ui/card/card-content";
 import { Separator } from "~/components/common/ui/separator";
 import { TabNavLink } from "~/components/navigation/tab-nav-link";
 
+import { getUser } from "~/services/user.server";
+
 import { honeypot } from "~/util/honeypot.server";
 
-export const loader = async () => {
+export const loader = async ({ request }: DataFunctionArgs) => {
+  const user = await getUser(request);
+
+  console.log(user);
+  if (user) {
+    return redirect("/");
+  }
+
   return json({ honeypotProps: honeypot.getInputProps() });
 };
 
 export default function LoginRegisterRoute() {
   const { honeypotProps } = useLoaderData<typeof loader>();
   return (
-    <main className="-mt-8 flex h-[calc(100vh-84px)] flex-col flex-wrap content-center gap-4 bg-accent pt-16 dark:bg-background sm:mt-0 sm:justify-center sm:pt-0">
+    <main className="-mt-8 flex h-[calc(100vh-84px)] flex-col flex-wrap content-center gap-4 bg-accent pt-16 sm:mt-0 sm:justify-center sm:pt-0 dark:bg-background">
       <HoneypotProvider {...honeypotProps}>
         <div className="w-full max-w-md px-2 sm:px-0">
           <Card className="min-h-[480px] border-none shadow-none sm:min-h-min sm:border sm:border-solid sm:shadow-sm">
