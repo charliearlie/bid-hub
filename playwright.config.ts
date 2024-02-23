2124a7bf7e0063c81d79d18225ecc7f768f53ab4
charlie <cw5790@gmail.com>
2024-02-23 20:12:43 +0000

    increase timeout for dev server to get running

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,6 +70,7 @@ export default defineConfig({
   webServer: {
     command: "PORT=7777 pnpm run dev",
     url: "http://localhost:7777",
+    timeout: 2 * 60 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 });
