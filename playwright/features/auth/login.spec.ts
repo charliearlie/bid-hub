ad4723ab3eb5a453cbcf91c131fdc9a8bf562641
charlie <cw5790@gmail.com>
2024-03-17 00:26:59 +0000

    Add further e2e tests

--- playwright/features/auth/login.spec.ts
+++ playwright/features/auth/login.spec.ts
@@ -37,3 +37,25 @@ test("shows an error if no username or email is input", async ({ page }) => {
 
   expect(page.getByText("Email or username is required")).toBeVisible();
 });
+
+test("handles incorrect password", async ({ page }) => {
+  await page.goto("/");
+
+  await page.getByRole("link", { name: "Log in" }).click();
+  await page.getByRole("main").getByRole("link", { name: "Log in" }).click();
+
+  await page.locator("#login-identifier-form-emailOrUsername").click();
+  await page
+    .locator("#login-identifier-form-emailOrUsername")
+    .fill(userData.email);
+
+  await page.getByRole("button", { name: "Next" }).click();
+
+  await page.locator("#login-challenge-form-password").click();
+
+  await page
+    .locator("#login-challenge-form-password")
+    .fill("incorrectpassword");
+
+  expect(page.getByText("Incorrect password")).toBeVisible();
+});
